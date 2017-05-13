ae323ce8372b3f0ba8716fcfd0cec8a6ca7cbddf
Masashi Motohashi <masashi6225@motohashisusumushi-no-MacBook-Pro.local>
2017-05-13 22:00:45 +0900

    The very first edition

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,40 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by Masashi Motohashi on 2017/05/13.
+//  Copyright © 2017 masashi.motohashi. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
